c8ef43416ca1e07b34c4f9ffbab1f291c227776d
Adam Cooke <iamadamcooke@gmail.com>
2013-09-17 20:51:58 -0400

    cleaned up code

--- js/map.js
+++ js/map.js
@@ -5,19 +5,19 @@
         var MILE_RADIUS = 1609;
         $(document).ready(function () {
             console.log('document ready');
-        function initialize() {
-            getDefaultLocation();
-            var mapOptions = {
-                zoom: 14,
-                center: loc,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            map = new google.maps.Map(document.getElementById('map-canvas'),
+            function initialize() {
+                getDefaultLocation();
+                var mapOptions = {
+                    zoom: 14,
+                    center: loc,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                map = new google.maps.Map(document.getElementById('map-canvas'),
                   mapOptions);
-            geocoder = new google.maps.Geocoder();
-            
-        }
-        initialize();
+                geocoder = new google.maps.Geocoder();
+                
+            }
+            initialize();
         //google.maps.event.addDomListener(window, 'load', initialize);
     });
         
@@ -32,105 +32,105 @@
         function createMarker(place) {
             var placeLoc = place.geometry.location;
             var marker = new google.maps.Marker({
-                                                map: map,
-                                                position: place.geometry.location
-                                                });
+                map: map,
+                position: place.geometry.location
+            });
             google.maps.event.addListener(marker, 'click', function() {
-                                          infowindow.setContent(place.name);
-                                          infowindow.open(map, this);
-                                          });
+              infowindow.setContent(place.name);
+              infowindow.open(map, this);
+          });
         }
 
         function getDefaultLocation() {
             if (navigator.geolocation) {
-                 navigator.geolocation.getCurrentPosition(success, error);
-            } else {
-                alert('geolocation not supported');
-            }
+               navigator.geolocation.getCurrentPosition(success, error);
+           } else {
+            alert('geolocation not supported');
         }
+    }
 
-        function success(position) {
-            loc = new google.maps.LatLng(position.coords.latitude,
-                                               position.coords.longitude);
-            map.setCenter(loc, 13);
-            newPlacesRequest(loc, MILE_RADIUS);
-            latLngToZip(loc);
-            
-            
-
-        }
-
-        function newPlacesRequest(location, radius) {
-            var typesToPlot = new Array();
-            if(document.getElementById('bar_checkbox').checked) {
-                typesToPlot.push('bar');
-            }
-            if(document.getElementById('liquorStore_checkbox').checked) {
-                typesToPlot.push('liquor_store');
-            }
-            if(document.getElementById('nightClub_checkbox').checked) {
-                typesToPlot.push('night_club');
-            }
-            
-            var request = {
-                location: location,
-                radius: radius,
-                types: typesToPlot
-            };
-            infowindow = new google.maps.InfoWindow();
-            var service = new google.maps.places.PlacesService(map);
-            service.nearbySearch(request, callback);
-        }
-
-        function error(msg) {
-
-            return alert('error: ' + msg);
-        }       
+    function success(position) {
+        loc = new google.maps.LatLng(position.coords.latitude,
+         position.coords.longitude);
+        map.setCenter(loc, 13);
+        newPlacesRequest(loc, MILE_RADIUS);
+        latLngToZip(loc);
+        
         
 
-        function getZip()
-        {
-            return document.getElementById('TEXTBOX_ID').value;
-            
-        }
-        
-        function IsNumeric(input)
-        {
-            return (input - 0) == input && (input+'').replace(/^\s+|\s+$/g, "").length > 0;
-        }
-        
-        function validateZip(zipcode) {
-            if (zipcode.length == 5 && IsNumeric(zipcode)){
-                return true;
-            }
-            return false;
-        }
+    }
 
-        function latLngToZip(location) {
-             geocoder.geocode({'latLng': location}, function (res, status) {  
-                var zipArray = res[0].formatted_address.match(/,\s\w{2}\s(\d{5})/);  
-                document.getElementById('TEXTBOX_ID').value = zipArray[1];
-                
-
-});  
-                    
+    function newPlacesRequest(location, radius) {
+        var typesToPlot = new Array();
+        if(document.getElementById('bar_checkbox').checked) {
+            typesToPlot.push('bar');
+        }
+        if(document.getElementById('liquorStore_checkbox').checked) {
+            typesToPlot.push('liquor_store');
+        }
+        if(document.getElementById('nightClub_checkbox').checked) {
+            typesToPlot.push('night_club');
         }
         
-        function plotZip(){
-            var zip = getZip();
-            if (validateZip(zip)){
-                geocoder.geocode( { 'address': zip}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
+        var request = {
+            location: location,
+            radius: radius,
+            types: typesToPlot
+        };
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+    }
+
+    function error(msg) {
+
+        return alert('error: ' + msg);
+    }       
+    
+
+    function getZip()
+    {
+        return document.getElementById('TEXTBOX_ID').value;
+        
+    }
+    
+    function IsNumeric(input)
+    {
+        return (input - 0) == input && (input+'').replace(/^\s+|\s+$/g, "").length > 0;
+    }
+    
+    function validateZip(zipcode) {
+        if (zipcode.length == 5 && IsNumeric(zipcode)){
+            return true;
+        }
+        return false;
+    }
+
+    function latLngToZip(location) {
+       geocoder.geocode({'latLng': location}, function (res, status) {  
+        var zipArray = res[0].formatted_address.match(/,\s\w{2}\s(\d{5})/);  
+        document.getElementById('TEXTBOX_ID').value = zipArray[1];
+        
+
+    });  
+       
+   }
+   
+   function plotZip(){
+    var zip = getZip();
+    if (validateZip(zip)){
+        geocoder.geocode( { 'address': zip}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
         //Got result, center the map and put it out there
         var newLocation = results[0].geometry.location;
         map.setCenter(newLocation);
         newPlacesRequest(newLocation, MILE_RADIUS);
-      } else {
+    } else {
         alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-            }
-            else {
-                alert('Please enter a Numeric 5 Digit ZIP code');
-            }
-        }
+    }
+});
+    }
+    else {
+        alert('Please enter a Numeric 5 Digit ZIP code');
+    }
+}
